bc76c2956871db6066bbb49d06111ee1d4b564e0
uniquelise01 <han19008@byui.edu>
2021-09-16 16:09:27 -0600

    added week01

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 document.getElementById("last-mod").innerHTML = document.lastModified;
+
+const links = [
+    {
+        label: "week1 notes",
+        url: "week01/index.html"
+    }
+]
