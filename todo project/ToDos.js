f3702066204de6283c693884005e27d8df0be9d8
uniquelise01 <han19008@byui.edu>
2021-10-08 20:52:34 -0600

    added w05 notes and practice

---
+++ todo project/ToDos.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//Create ToDos class
+class ToDos {
+
+}
+
+
+//Create saveTodo(task, key)
+
+
+//Create getTodos(key) function
+
+
+//Complete Todos.addTodo()
+
+
+//Bind Todos.addTodo to the Add button
+
+
+//Create the renderTodoList(list, element) funtion
+
+
+//Complete Todos.listTodos()
+
+
+//Complete Todos.completeTodo()
+
+
+//Complete Todos.removeTodo()
+
+
+//Complete Todos.filterTodos()
